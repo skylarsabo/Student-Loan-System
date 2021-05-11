3ef6c94e42981b51a4088ff360f9a2e0c39d1f88
Christian Novak <cnovak487@gmail.com>
2021-05-11 19:14:22 -0400

    Calculator Classes
    Started basic calculator classes

---
+++ obj/PaymentCalculator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentLoanProgram
+{
+    class PaymentCalculator
+    {
+        var loanPrinciple = "";
+        var loanInterestRate = "";
+        var loanPaymentPeriod = "";
+        
+        public string getLoanPrinciple() { return loanPrinciple; }
+
+        public string getLoanInterestRate() { return loanInterestRate; }
+
+        public string getLoanPaymentPeriod() { return loanPaymentPeriod; }
+
+        private void setLoanPrinciple(string newLoanPrinciple) { loanPrinciple = newLoanPrinciple; }
+
+        private void setLoanInterestRate(string newLoanInterestRate) { loanInterestRate = newLoanInterestRate; }
+
+        private void setLoanPaymentPeriod(string newLoanPaymentPeriod) { loanPaymentPeriod = newLoanPaymentPeriod; }
+
+        public int calculatePayments(string loanPrinciple, string loanInterestRate, string loanPaymentPeriod)
+        {
+            int paymentDue = 0;
+            return paymentDue;
+        }
+
+    }
+
+}
---
+++ obj/RiskCalculator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentLoanProgram
+{
+    class RiskCalculator
+    {
+        var studentCreditScore = "";
+        var studentReferences = "";
+        var studentInformation = "";
+
+        public string getStudentCreditScore() { return studentCreditScore; }
+
+        public string getStudentReferences() { return studentReferences; }
+
+        public string getStudentInformation() { return studentInformation; }
+
+        private void setStudentCreditScore(string newStudentCreditScore) { studentCreditScore = newStudentCreditScore; }
+
+        private void setStudentReferences(string newStudentReferences) { studentReferences = newStudentReferences; }
+
+        private void setStudentInformation(string newStudentInformation) { studentInformation = newStudentInformation; }
+
+        public int calculateRisk(string studentCreditScore, string studentReferences, string studentInformation)
+        {
+            int calculatedRisk = 0;
+            return calculatedRisk;
+        }
+
+    }
+
+}
