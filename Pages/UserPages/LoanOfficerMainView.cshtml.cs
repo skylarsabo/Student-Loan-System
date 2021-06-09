93eab0991cd928ca706a826bdc051f818bbbe36b
NateHarder <hard5340@kettering.edu>
2021-06-09 12:04:42 -0700

    Replace CheckBoxes With RadioButtons.

--- Pages/UserPages/LoanOfficerMainView.cshtml.cs
+++ Pages/UserPages/LoanOfficerMainView.cshtml.cs
@@ -19,17 +19,18 @@ namespace StudentLoanSystem.Pages.UserPages
             StudentDatabase = AccountData.RetrieveStudentList();
             LoanDatabase = AccountData.RetrieveLoanList();
             approved = new List<Loan>(LoanDatabase.Where(loan => loan.Approved == 1));
+            denied = new List<Loan>(LoanDatabase.Where(loan => loan.Approved == 2));
             unapproved = new List<Loan>(LoanDatabase.Where(loan => loan.Approved == 0));
-            AboutToApprove = new bool[LoanDatabase.Count];
+            ApproveArray = new string[unapproved.Count];
         }
 
         public List<StudentLoanSystem.Data.Loan> LoanDatabase { get; set; }
         public List<StudentLoanSystem.Data.Users.Student> StudentDatabase { get; set; }
         public List<Loan> approved { get; set; }
+        public List<Loan> denied { get; set; }
         public List<Loan> unapproved { get; set; }
-        public bool[] AboutToApprove { get; set; }
         [BindProperty]
-        public List<string> ApproveArray { get; set; }
+        public string[] ApproveArray { get; set; }
 
         public async Task OnGet()
         {
@@ -37,44 +38,66 @@ namespace StudentLoanSystem.Pages.UserPages
 
         public async Task<IActionResult> OnPostAsync()
         {
-            for(var i = 0; i < ApproveArray.Count; i++) {
-                if (ApproveArray.ElementAt(i) == "on") {
+            for (var i = 0; i < ApproveArray.Length; i++)
+            {
+                if (ApproveArray.ElementAt(i) == "Approve")
+                {
                     this.unapproved.ElementAt(i).Approved = 1;
+                }
+                else if (ApproveArray.ElementAt(i) == "Deny") 
+                {
+                    this.unapproved.ElementAt(i).Approved = 2;
                 }
             }
 
             SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            builder.ConnectionString = "Server=tcp:student-loan-server.database.windows.net,1433;Initial Catalog=student-loan-db;Persist Security Info=False;User Id=student-loan-admin;Password=Password123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            string SQLDataSource = "student-loan-server.database.windows.net";
-            string SQLUserID = "student-loan-admin";
-            string SQLPassword = "Password123";
-            string SQLInitialCatalog = "student-loan-db";
-            builder.DataSource = SQLDataSource;
-            builder.UserID = SQLUserID;
-            builder.Password = SQLPassword;
-            builder.InitialCatalog = SQLInitialCatalog;
+            builder.DataSource = "student-loan-server.database.windows.net";
+            builder.UserID = "student-loan-admin";
+            builder.Password = "Password123";
+            builder.InitialCatalog = "student-loan-db";
 
             using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
             {
+                connection.Open();
                 String sql = "UPDATE LoanTable SET approved='1' WHERE Id=";
                 foreach (var item in this.unapproved.ToList())
                 {
                     if (item.Approved == 1) {
                         sql += "'" + item.Id.ToString() + "' OR Id=";
-                      this.unapproved.Remove(item);
-                      this.approved.Add(item);
+                        this.unapproved.Remove(item);
+                        this.approved.Add(item);
                     }
                 }
-                
-                sql = sql.Substring(0, sql.Length - 7);
-               // sql += ";";
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
+                if (sql.EndsWith("' OR Id="))
                 {
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                    connection.Close();
+                    sql = sql.Substring(0, sql.Length - 7);
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
                 }
+
+                sql = "UPDATE LoanTable SET approved='2' WHERE Id=";
+                foreach (var item in this.unapproved.ToList())
+                {
+                    if (item.Approved == 2)
+                    {
+                        sql += "'" + item.Id.ToString() + "' OR Id=";
+                        this.unapproved.Remove(item);
+                        this.denied.Add(item);
+                    }
+                }
+                if (sql.EndsWith("' OR Id="))
+                {
+                    sql = sql.Substring(0, sql.Length - 7);
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+                connection.Close();
             }
             return Page();
         }
